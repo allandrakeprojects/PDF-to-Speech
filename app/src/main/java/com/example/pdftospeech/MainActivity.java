5a25aba0aaac6f4ad2199002ec17b4bdc8cc23ad
Allan Drake Paladin Dulay <40012648+allandrakeprojects@users.noreply.github.com>
2019-08-17 16:06:42 +0800

    Add PDF, Clear/Refresh, Button Controls, Settings Save Offline

--- app/src/main/java/com/example/pdftospeech/MainActivity.java
+++ app/src/main/java/com/example/pdftospeech/MainActivity.java
@@ -1,32 +1,67 @@
 package com.example.pdftospeech;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.AudioManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
-import android.support.v4.view.GravityCompat;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.view.MenuItem;
-import android.support.design.widget.NavigationView;
+import android.preference.PreferenceManager;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
 import android.support.v4.widget.DrawerLayout;
-
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-import android.widget.Button;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import com.github.barteksc.pdfviewer.PDFView;
+import com.github.barteksc.pdfviewer.util.FitPolicy;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.PdfTextExtractor;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.DexterError;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.PermissionRequestErrorListener;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.nbsp.materialfilepicker.MaterialFilePicker;
+import com.nbsp.materialfilepicker.ui.FilePickerActivity;
 
-    // Buttons
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Pattern;
 
-    // TextView
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, TextToSpeech.OnInitListener {
 
     // Button
-    ImageButton buttonAddPDF;
+    private ImageButton buttonAddPDF;
+
+    // ImageView
+    private ImageView buttonPlay, buttonPause, buttonStop, buttonNext, buttonPrevious;
+
+    // LinearLayout
+    private LinearLayout linearLayoutButtons;
+
+    // PDFView
+    private PDFView pdfView;
+
+    // TTS
+    private TextToSpeech textToSpeech;
+
+    // Variables
+    private boolean IsPDFView;
+    private String FilePath;
+    private String PageContent;
+    private int PageCount;
+    private int CurrentPage = 1;
 
     /**
      * Create the app
@@ -35,6 +70,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RequestMultiplePermission();
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -47,9 +85,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      * Initialize Widgets
      */
     private void INITIALIZE(){
-        // Button Add PDF
+        // Button
         buttonAddPDF = findViewById(R.id.buttonAddPDF);
+
+        // ImageView
+        buttonPlay = findViewById(R.id.buttonPlay);
+        buttonPause = findViewById(R.id.buttonPause);
+        buttonStop = findViewById(R.id.buttonStop);
+        buttonNext = findViewById(R.id.buttonNext);
+        buttonPrevious = findViewById(R.id.buttonPrevious);
+
+        // Listener
+        buttonPlay.setOnClickListener(this);
+        buttonPause.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
+        buttonNext.setOnClickListener(this);
+        buttonPrevious.setOnClickListener(this);
         buttonAddPDF.setOnClickListener(this);
+
+        // LinearLayout
+        linearLayoutButtons = findViewById(R.id.linearLayoutButtons);
+
+        // PDFView
+        pdfView = findViewById(R.id.pdfView);
+
+        // TTS
+        textToSpeech = new TextToSpeech(this, this);
     }
 
     /**
@@ -59,7 +120,83 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         // Button Add PDF
         if(view.getId() == R.id.buttonAddPDF){
-            Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
+            new MaterialFilePicker()
+                .withActivity(this)
+                .withRequestCode(1000)
+                .withFilter(Pattern.compile(".*\\.pdf$")) // Filtering files and directories by file name using regexp
+                .withFilterDirectories(false) // Set directories filterable (false by default)
+                .withHiddenFiles(true) // Show hidden files and folders
+                .start();
+        }
+        // Button Play
+        else if(view.getId() == R.id.buttonPlay){
+            ButtonPlay();
+        }
+        // Button Pause
+        else if(view.getId() == R.id.buttonPause){
+            ButtonPause();
+        }
+        // Button Stop
+        else if(view.getId() == R.id.buttonStop){
+            ButtonStop();
+        }
+        // Button Previous
+        else if(view.getId() == R.id.buttonPrevious){
+            ButtonPrevious();
+        }
+        // Button Next
+        else if(view.getId() == R.id.buttonNext){
+            ButtonNext();
+        }
+    }
+
+    /**
+     * Button Play
+     */
+    private void ButtonPlay(){
+        buttonPlay.setVisibility(View.GONE);
+        buttonPause.setVisibility(View.VISIBLE);
+        DetailsPDF();
+        Speak();
+    }
+
+    /**
+     * Button Pause
+     */
+    private void ButtonPause(){
+        buttonPlay.setVisibility(View.VISIBLE);
+        buttonPause.setVisibility(View.GONE);
+
+    }
+
+    /**
+     * Button Stop
+     */
+    private void ButtonStop(){
+        buttonPlay.setVisibility(View.VISIBLE);
+        buttonPause.setVisibility(View.GONE);
+        textToSpeech.stop();
+    }
+
+    /**
+     * Button Previous
+     */
+    private void ButtonPrevious(){
+        if(CurrentPage != 1){
+            CurrentPage--;
+
+            pdfView.jumpTo(CurrentPage-1);
+        }
+    }
+
+    /**
+     * Button Next
+     */
+    private void ButtonNext(){
+        if(CurrentPage < PageCount){
+            CurrentPage++;
+
+            pdfView.jumpTo(CurrentPage-1);
         }
     }
 
@@ -82,6 +219,95 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
+
+
+
+
+
+
+
+
+    /**
+     * Handle File Path of Selected PDF
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1000 && resultCode == RESULT_OK) {
+            FilePath = data.getStringExtra(FilePickerActivity.RESULT_FILE_PATH);
+            File file = new File(FilePath);
+            PDFView pdfView = findViewById(R.id.pdfView);
+            pdfView.fromFile(file)
+                .enableSwipe(false)
+                .swipeHorizontal(true)
+                .enableDoubletap(true)
+                .defaultPage(0)
+                .enableAnnotationRendering(false)
+                .password(null)
+                .scrollHandle(null)
+                .enableAntialiasing(true)
+                .spacing(0)
+                .pageFitPolicy(FitPolicy.WIDTH)
+                .load();
+
+            String filename = file.getName();
+            // Toast.makeText(this, filename, Toast.LENGTH_SHORT).show();
+            pdfView.setVisibility(View.VISIBLE);
+            linearLayoutButtons.setVisibility(View.VISIBLE);
+            buttonAddPDF.setVisibility(View.GONE);
+
+            IsPDFView = true;
+
+            DetailsPDF();
+        }
+    }
+
+    private void DetailsPDF(){
+        try {
+            String parsedText = "";
+            PdfReader reader = new PdfReader(FilePath);
+            PageCount = reader.getNumberOfPages();
+            PageContent = parsedText + PdfTextExtractor.getTextFromPage(reader, CurrentPage).trim(); //Extracting the content from the different page
+            reader.close();
+        } catch (Exception e) {
+            Toast.makeText(this, "Error:" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Initialize Multiple Permission
+     */
+    private void RequestMultiplePermission(){
+        Dexter.withActivity(this)
+            .withPermissions(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)
+            .withListener(new MultiplePermissionsListener() {
+                @Override
+                public void onPermissionsChecked(MultiplePermissionsReport report) {
+                    if (report.areAllPermissionsGranted()) {
+                    }
+
+                    if (report.isAnyPermissionPermanentlyDenied()) {
+                        Toast.makeText(MainActivity.this, "To able to access the file on your phone. Allow the permission.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                    token.continuePermissionRequest();
+                }
+            }).
+            withErrorListener(new PermissionRequestErrorListener() {
+                @Override
+                public void onError(DexterError error) {
+                    Toast.makeText(getApplicationContext(), "Some Error! ", Toast.LENGTH_SHORT).show();
+                }
+            })
+            .onSameThread()
+            .check();
+    }
 
     /**
      * Initialize Settings Menu
@@ -109,8 +335,98 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(this, SettingsActivity.class);
             startActivity(intent);
             return true;
+        } else if (id == R.id.action_refresh) {
+            if(IsPDFView){
+                ButtonStop();
+
+                pdfView.setVisibility(View.GONE);
+                linearLayoutButtons.setVisibility(View.GONE);
+                buttonAddPDF.setVisibility(View.VISIBLE);
+
+                pdfView.invalidate();
+
+                IsPDFView = false;
+            } else{
+                Toast.makeText(this, "You must select PDF first.", Toast.LENGTH_SHORT).show();
+            }
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Initialize TTS
+     */
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            int result = textToSpeech.setLanguage(Locale.US);
+            if (result == TextToSpeech.LANG_MISSING_DATA
+                    || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Toast.makeText(getApplicationContext(), "Language is not supported on this device.", Toast.LENGTH_SHORT).show();
+            } else {
+                Speak();
+            }
+
+            textToSpeech.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+                @Override
+                public void onDone(String utteranceId) {
+                    Toast.makeText(MainActivity.this, "asdsadasdsa done", Toast.LENGTH_SHORT).show();
+                    buttonPlay.setVisibility(View.VISIBLE);
+                    buttonPause.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onError(String utteranceId) {
+                }
+
+                @Override
+                public void onStart(String utteranceId) {
+                }
+            });
+        } else {
+            Toast.makeText(getApplicationContext(), "Language is not supported on this device.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Callback Detect Speak When Finished
+     */
+//    public void onUtteranceCompleted(String utteranceId) {
+//        SetButtonEnable();
+//    }
+
+    public void SetButtonEnable(){
+    }
+
+    /**
+     * Function Speak
+     */
+    private void Speak() {
+        HashMap<String, String> hashMapAlarm = new HashMap<>();
+        hashMapAlarm.put(TextToSpeech.Engine.KEY_PARAM_STREAM, String.valueOf(AudioManager.STREAM_ALARM));
+        hashMapAlarm.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "-");
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        // Speed
+        Float speed = settings.getFloat("TTS_SPEED", 20);
+        textToSpeech.setSpeechRate(speed);
+        // Pitch
+        Float pitch = settings.getFloat("TTS_PITCH", 20);
+        textToSpeech.setPitch(pitch);
+
+        textToSpeech.speak(PageContent, TextToSpeech.QUEUE_FLUSH, hashMapAlarm);
+    }
+
+    /**
+     * Destroy TTS
+     */
+    @Override
+    public void onDestroy() {
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/example/pdftospeech/SettingsActivity.java
+++ app/src/main/java/com/example/pdftospeech/SettingsActivity.java
@@ -1,19 +1,57 @@
 package com.example.pdftospeech;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.xw.repo.BubbleSeekBar;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    // SeekBar
+    BubbleSeekBar seekBarSpeed, seekBarPitch;
+
+    /**
+     * Create the app
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         setTitle("Settings");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        INITIALIZE();
     }
 
+    /**
+     * Initialize Widgets
+     */
+    private void INITIALIZE(){
+        // SeekBar
+        seekBarSpeed = findViewById(R.id.seekBarSpeed);
+        seekBarPitch = findViewById(R.id.seekBarPitch);
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(SettingsActivity.this);
+        // Speed
+        Float speed = settings.getFloat("TTS_SPEED", 20);
+        if(!speed.toString().contains("20")){
+            seekBarSpeed.setProgress(speed);
+        }
+        // Pitch
+        Float pitch = settings.getFloat("TTS_PITCH", 20);
+        if(!pitch.toString().contains("20")){
+            seekBarPitch.setProgress(pitch);
+        }
+    }
+
+    /**
+     * Initialize Back Listener
+     */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -23,5 +61,26 @@ public class SettingsActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    /**
+     * Function to Save Speed, Pitch Settings
+     */
+    public void SaveTTSDetails() {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(SettingsActivity.this);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putFloat("TTS_SPEED", seekBarSpeed.getProgressFloat());
+        editor.putFloat("TTS_PITCH", seekBarPitch.getProgressFloat());
+        editor.commit();
+    }
+
+    /**
+     * Override onBackPressed to Save Speed, Pitch Settings
+     */
+    @Override
+    public void onBackPressed() {
+        SaveTTSDetails();
+        setResult(RESULT_CANCELED);
+        super.onBackPressed();
     }
 }
